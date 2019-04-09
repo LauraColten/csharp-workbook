dc2d283f10133a7f92efe7175d93b5c86835ed1c
Laura Colten <lauracolten5@gmail.com>
2019-04-09 18:55:16 -0500

    positive and negative

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Laura!");
+            Console.WriteLine("Plese enter a number: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            if (userNum > 0) {
+                Console.WriteLine("Your number is positive.");
+            }else {
+                Console.WriteLine("Your number is negative.");
+            }
+            }
+
         }
     }
-}
+
