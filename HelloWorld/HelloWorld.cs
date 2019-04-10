66750b08d9d2ee0e89e2c081afac952a58c38d1f
Laura Colten <lauracolten5@gmail.com>
2019-04-09 19:04:02 -0500

    formatted braces

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -2,19 +2,22 @@
 
 namespace HelloWorld
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Plese enter a number: ");
-            int userNum = Convert.ToInt32(Console.ReadLine());
-            if (userNum > 0) {
-                Console.WriteLine("Your number is positive.");
-            }else {
-                Console.WriteLine("Your number is negative.");
-            }
-            }
-
-        }
+      Console.WriteLine("Plese enter a number: ");
+      int userNum = Convert.ToInt32(Console.ReadLine());
+      if (userNum > 0)
+      {
+        Console.WriteLine("Your number is positive.");
+      }
+      else
+      {
+        Console.WriteLine("Your number is negative.");
+      }
     }
+
+  }
+}
 
