18b08cec87a455bf372a0f021b24441968521da0
Laura Colten <lauracolten5@gmail.com>
2019-04-09 22:59:00 -0500

    rps

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -6,20 +6,44 @@ namespace RockPaperScissors
     {
         public static void Main()
         {
-            Console.WriteLine("Enter hand 1:");
+            Console.WriteLine("Let's play rock, paper, scissors!");
+            Console.WriteLine("Enter r, p, or s");
             string hand1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Enter hand 2:");
-            string hand2 = Console.ReadLine().ToLower();
-            Console.WriteLine(CompareHands(hand1, hand2));
+            Console.WriteLine("The computer is choosing...");
+            string newChoice = computerChoice();
+            Console.WriteLine(newChoice);
+            Console.WriteLine(CompareHands(hand1, newChoice));
 
-            // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
         }
-        
+
+
+        public static string computerChoice()
+        {
+            Random generator = new Random();
+            int randomNumber = generator.Next(0, 3);
+            string numToString = randomNumber.ToString();
+            if (numToString == "0") {
+                return "r";
+            }else if (numToString == "1") {
+                return "p";
+            }else{
+                return "s";
+            }
+
+        }
         public static string CompareHands(string hand1, string hand2)
         {
-            // Your code here
-            return hand1 + ' ' + hand2;
+            if (hand1 == hand2) {
+                return "It's a tie!";
+            }else if (hand1 == "r" && hand2 == "s") {
+                return "You win!";
+            }else if (hand1 == "s" && hand2 == "p") {
+                return "You win!";
+            }else if (hand1 == "p" && hand2 == "r") {
+                return "You win!";
+            }else {
+                return "The computer wins!";
+            }
         }
     }
 }
