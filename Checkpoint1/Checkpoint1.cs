9594f712b7f45f9710ebae5a12e733c8d830eb39
Laura Colten <lauracolten5@gmail.com>
2019-04-25 17:14:03 -0500

    first 3

--- Checkpoint1/Checkpoint1.cs
+++ Checkpoint1/Checkpoint1.cs
@@ -6,7 +6,39 @@ namespace Checkpoint1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = 0;
+            for(int i = 0; i < 101; i++) {
+                if (i % 3 == 0) {
+                    Console.WriteLine(i);
+                    count++;
+                }
+            }
+            Console.WriteLine("The count of numbers divisible by 3 is: " +count);
+
+            string answer = " ";
+            int sum = 0;
+            while (answer != "ok") {
+                Console.WriteLine("Enter a number or 'ok' to exit: ");
+                answer = Console.ReadLine();
+                if(answer == "ok") {
+                    break;
+                }
+                sum += Convert.ToInt32(answer);
+            }
+            Console.WriteLine("The sum of the entered numbers is: " +sum);
+
+            int total = 1;
+            Console.WriteLine("Please enter a number: ");
+            string num = Console.ReadLine();
+            int intNum = Convert.ToInt32(num);
+            for (int i = intNum; i > 0; i--) {
+                total *= i;
+            }
+            Console.WriteLine(intNum+ "! = " +total);
+
+
+
         }
+
     }
 }
