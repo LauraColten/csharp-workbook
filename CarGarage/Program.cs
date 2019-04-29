02866c3a07be64fcadaeb2f89d72c823283dc5a7
Laura Colten <lauracolten5@gmail.com>
2019-04-28 21:42:19 -0500

    first commit

--- CarGarage/Program.cs
+++ CarGarage/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarGarage
 {
-    class Program
+  class Program
+  {
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
+      Car blueCar = new Car("blue");
+      Garage smallGarage = new Garage(2);
 
-        }
+      smallGarage.ParkCar(blueCar, 0);
+      Console.WriteLine(smallGarage.Cars);
+
+      List<Person> people = new List<Person>();
+      for (int i = 0; i < 3; i++)
+      {
+        Person x = new Person("Arlo");
+        Console.WriteLine("Enter your name: ");
+        x.Name = Console.ReadLine();
+        people.Add(x);
+      }
     }
+  }
+
+  class Car
+  {
+    public Car(string initialColor)
+    {
+      Color = initialColor;
+    }
+
+    public string Color { get; private set; }
+  }
+
+  class Garage
+  {
+    private Car[] cars;
+
+    public Garage(int initialSize)
+    {
+      Size = initialSize;
+      this.cars = new Car[initialSize];
+    }
+
+    public int Size { get; private set; }
+
+    public void ParkCar(Car car, int spot)
+    {
+      cars[spot] = car;
+    }
+
+    public string Cars
+    {
+      get
+      {
+        for (int i = 0; i < cars.Length; i++)
+        {
+          if (cars[i] != null)
+          {
+            Console.WriteLine(String.Format("The {0} car is in spot {1}.", cars[i].Color, i));
+          }
+        }
+        return "That's all!";
+      }
+    }
+  }
+
+  class Person
+  {
+    public Person(string firstName)
+    {
+      Name = firstName;
+    }
+    public string Name { get; set; }
+  }
+
 }
---
+++ OOP1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      List<Person> people = new List<Person>();
+      for (int i = 0; i < 3; i++)
+      {
+
+        Person x = new Person();
+        Console.WriteLine("Enter your first name: ");
+        x.firstName = Console.ReadLine();
+        Console.WriteLine("Enter your last name: ");
+        x.lastName = Console.ReadLine();
+        Console.WriteLine("Enter your DOB: ");
+        x.dOb = Console.ReadLine();
+        people.Add(x);
+      }
+
+      for (int i = 0; i < people.Count; i++)
+      {
+        Person w = people[i];
+        string FullName = Person.StaticFullName(w.firstName, w.lastName);
+        Console.WriteLine("Full Name: {0}", FullName); ;
+        Console.WriteLine("DOB: {0}", w.dOb);
+      }
+    }
+
+    Person p0 = people[0];
+    string FullName0 = p0.NonStaticFullName();
+  }
+
+
+  class Person
+  {
+    public string firstName;
+    public string lastName;
+    public string dOb;
+
+    public static string StaticFullName(string f, string l)
+    {
+      string full = f + " " + l;
+      return full;
+    }
+
+    public string NonStaticFullName() {
+        return this.firstName+" "+this.lastName;
+    }
+
+  }
+
+}
