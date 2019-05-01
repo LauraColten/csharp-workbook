c44701c52992ae23313fb6c3a485c60b2abe1565
Laura Colten <lauracolten5@gmail.com>
2019-04-30 19:20:59 -0500

    class practice

---
+++ ClassPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Person
+    {
+        public char gender;
+        public string name;
+
+        public Person(bool chooseGender) {
+            if (chooseGender) {
+                this.name = "Jane Doe";
+                this.gender = 'F';
+            }else {
+                this.name = "John Doe";
+                this.gender = 'M';
+            }
+        }
+    }
+}
