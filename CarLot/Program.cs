a456bc1b725c75056311a5a7f7a9145b388afb44
Laura Colten <lauracolten5@gmail.com>
2019-05-05 17:49:00 -0500

    finished

--- CarLot/Program.cs
+++ CarLot/Program.cs
@@ -8,14 +8,25 @@ namespace CarLot
         static void Main(string[] args)
         {
             CarLot usedCars = new CarLot("Used Cars");
-            Car mazda = new Car(//car attributes go here);
-            Car bmw = new Car();
-            Truck f150 = new Truck();
+            Car mazda = new Car("ABC123", "Mazda", "CX9", 27000, "SUV", 4);
+            Car bmw = new Car("DEF456", "BMW", "X5", 65000, "SUV", 4);
+            Truck f150 = new Truck("GHI789", "Ford", "F150", 35000, "Standard");
             usedCars.Add(mazda);
             usedCars.Add(bmw);
             usedCars.Add(f150);
 
             usedCars.PrintInventory();
+
+            CarLot newCars = new CarLot("New Cars");
+            Car tesla = new Car("JKL890", "Tesla", "Model X", 109000, "SUV", 4);
+            Car honda = new Car("MNO098", "Honda", "Clarity", 38000, "Sedan", 4);
+            Truck tundra = new Truck("PQR987", "Toyota", "Tundra", 46000, "Extended");
+            newCars.Add(tesla);
+            newCars.Add(honda);
+            newCars.Add(tundra);
+
+            newCars.PrintInventory();
+
         }
     }
 
@@ -23,14 +34,22 @@ namespace CarLot
     //represents a car lot
     {
         public string name;
-        public List<Vehicle> inventory;
+
+        public CarLot(string initialName)
+        {
+            this.name = initialName;
+        }
+        public List<Vehicle> inventory = new List<Vehicle>();
 
         public void Add(Vehicle vehicle) {
-            //code goes here
+            inventory.Add(vehicle);
         }
 
         public void PrintInventory() {
-            //code goes here
+            for (int i = 0; i < inventory.Count; i++) {
+                Console.WriteLine(inventory[i].ToString());
+
+            }
         }
     }
     public abstract class Vehicle
@@ -41,20 +60,47 @@ namespace CarLot
         public string model;
         public double price;
 
-        public string ToString() {
-            //code goes here
-            return s;
+         public Vehicle(string initialLicensePlate, string initialMake, string initalModel, double initialPrice)
+        {
+            this.licensePlate = initialLicensePlate;
+            this.make = initialMake;
+            this.model = initalModel;
+            this.price = initialPrice;
         }
+
+
     }
     public class Car : Vehicle
     //child class that extends vehicle
     {
 
+        public string type;
+        public int numberDoors;
+
+        public Car(string licensePlate, string make, string model, double price, string initialType, int initialNumberDoors) : base(licensePlate, make, model, price)
+        {
+            this.type = initialType;
+            this.numberDoors = initialNumberDoors;
+        }
+
+         override public string ToString() {
+            string s = string.Format("({0}), ({1}), ({2}), ({3}), ({4}), ({5})", this.make, this.model, this.licensePlate, this.price, this.type, this.numberDoors);
+            return s;
+         }
     }
     public class Truck : Vehicle
     //child class that extends vehicle
     {
+        public string bedSize;
+        public Truck(string licensePlate, string make, string model, double price, string initialBedSize) : base(licensePlate, make, model, price)
+        {
+            this.bedSize = initialBedSize;
+        }
 
+         override public string ToString() {
+            string s = string.Format("({0}), ({1}), ({2}), ({3}), ({4})", this.make, this.model, this.licensePlate, this.price, this.bedSize);
+            return s;
+        }
     }
-
 }
+
