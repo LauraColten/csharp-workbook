6dd3719f10b039d20819ff7a05b0aea4fc6fee07
Laura Colten <lauracolten5@gmail.com>
2019-05-07 19:01:02 -0500

    polymorph practice

---
+++ HumanClass/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HumanClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h1 = new Human("John", "Jack");
+            SuperHuman s1 = new SuperHuman("Superman", "Clark Kent", "speed");
+            Console.WriteLine(h1.getGreeting());
+            Console.WriteLine(s1.getGreeting());
+
+
+        }
+    }
+
+    class Human
+    {
+        public string name;
+        public string nickname;
+
+        public Human(string initialName, string initialNickname)
+        {
+            this.name = initialName;
+            this.nickname = initialNickname;
+
+        }
+
+        public virtual string getGreeting() {return "Hi, my name is " +name+ ", but you can call me: " +nickname;}
+
+    }
+
+    class SuperHuman : Human
+    {
+        public string superPower;
+
+        public SuperHuman(string name, string nickname, string initialSuperPower) : base(name, nickname)
+        {
+            this.superPower = initialSuperPower;
+        }
+
+        public override string getGreeting() { return "Hi, my name is " +name+ ", my nickname is " +nickname+ ", and my super power is: " +superPower;}
+
+    }
+}
