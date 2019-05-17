519f5c67bc48e6deeb9faad69eafb68f9c697c23
Laura Colten <lauracolten5@gmail.com>
2019-05-16 20:48:28 -0500

    finished

--- linqpractice/Program.cs
+++ linqpractice/Program.cs
@@ -13,13 +13,43 @@ namespace linqpractice
             students.Add(new Student("Eve", 6));
             students.Add(new Student("Arlo", 2));
 
-            IEnumerable<Student> lessThansix = students.Where(s => s.age < 6);
+            IEnumerable<Student> lessThanSix = students
+                        .Where(s => s.age < 33)
+                        .Where(x => x.name.StartsWith("A"));
+
+            IEnumerable<String> list =
+                from s in students
+                where s.age < 5
+                where s.name.StartsWith("E")
+                select s.name;
+
+            IEnumerable<string> lessThanSixOnly = students
+                        .Where(s => s.age < 33)
+                        .Where(x => x.name.StartsWith("A"))
+                        .Select(s => s.name);
+
+            IEnumerable<string> lessThanSixOnly2 = students
+                        .Where(s => s.age < 33) //student goes in, student comes out
+                        .Select(s => s.name) //string goes in, string comes out
+                        .Where(x => x.StartsWith("E") || x.StartsWith("A"));// student goes in, string comes out
+
+
+            foreach(string s in lessThanSixOnly2)
+            {
+                Console.WriteLine(s);
+            }
         }
 
-        bool filter(Student s)
-        {
-            return s.age < 6;
-        }
+        // bool filter(Student s)
+        // {
+        //     return s.age < 6;
+        // }
+
+        // bool filter2(Student x)
+        // {
+        //     return x.name.StartsWith("E");
+        // }
+
     }
 
     class Student
