2a4eb6fd2900bdf957942dce3137824dce31ab22
Laura Colten <lauracolten5@gmail.com>
2019-05-16 20:09:38 -0500

    first commit

---
+++ linqpractice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace linqpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student("Eden", 12));
+            students.Add(new Student("Eve", 6));
+            students.Add(new Student("Arlo", 2));
+
+            IEnumerable<Student> lessThansix = students.Where(s => s.age < 6);
+        }
+
+        bool filter(Student s)
+        {
+            return s.age < 6;
+        }
+    }
+
+    class Student
+    {
+        public string name;
+        public int age;
+        public Student(string iName, int iAge)
+        {
+            this.name = iName;
+            this.age = iAge;
+        }
+    }
+}
